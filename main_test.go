84cae9b22796ceeaffbac1db5367b65aaf32bb2e
Lee Spottiswood <me@lee.io>
2025-02-26 20:59:42 +0000

    select tests

--- main_test.go
+++ main_test.go
@@ -33,23 +33,51 @@ func TestContains(t *testing.T) {
 	})
 }
 
+func TestSelect(t *testing.T) {
+	type teststruct struct {
+		Property1 string
+		Property2 int
+	}
+	var teststructs []teststruct
+	teststructs = append(teststructs, teststruct{
+		Property1: "s1",
+		Property2: 1,
+	}, teststruct{
+		Property1: "s2",
+		Property2: 2,
+	})
+	c := gocollection.NewFromSlice(teststructs)
+
+	t.Run("ReturnProperty", func(t *testing.T) {
+		var results []int
+		for _, val := range c.Select(func(x teststruct) any {
+			return x.Property2
+		}).Slice() {
+			results = append(results, val.(int))
+		}
+
+		assert.Equal(t, 1, results[0])
+		assert.Equal(t, 2, results[1])
+	})
+}
+
 // println(c.Where(func(x string) bool {
 // 	return x == "b"
 // }).First())
 
 // println(c.Last())
 
-// var somestructs []somestruct
-// somestructs = append(somestructs, somestruct{
+// var teststructs []teststruct
+// teststructs = append(teststructs, teststruct{
 // 	Property1: "s1",
 // 	Property2: 1,
-// }, somestruct{
+// }, teststruct{
 // 	Property1: "s2",
 // 	Property2: 2,
 // })
 
-// s := golinq.NewFromSlice(somestructs)
-// for _, val := range s.Select(func(x somestruct) any {
+// s := golinq.NewFromSlice(teststructs)
+// for _, val := range s.Select(func(x teststruct) any {
 // 	return x.Property1
 // }).Slice() {
 // 	println(val.(string))
